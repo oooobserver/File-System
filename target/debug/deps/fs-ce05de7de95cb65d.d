5d5464e95ac8754842eabac414ceb95dbfcc4184
shenye <shenye@shenyes-MacBook-Air.local>
2023-10-30 15:49:59 +0800

    first

---
+++ target/debug/deps/fs-ce05de7de95cb65d.d
@@ -0,0 +1,5 @@
+/Users/shenye/Desktop/rust/fs/target/debug/deps/fs-ce05de7de95cb65d: src/main.rs
+
+/Users/shenye/Desktop/rust/fs/target/debug/deps/fs-ce05de7de95cb65d.d: src/main.rs
+
+src/main.rs:
---
+++ target/debug/fs.d
@@ -0,0 +1,1 @@
+/Users/shenye/Desktop/rust/fs/target/debug/fs: /Users/shenye/Desktop/rust/fs/src/main.rs
